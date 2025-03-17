bb817f5d280d31e2d7f6b4f9fd0cc2d9d6741722
DanielNkunzimana <mcdanielgolden11@gmail.com>
2025-03-17 20:37:42 +0200

    create image, text and content

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Daniel Nkunzimana")
+    content = """
+    Hi I'm Daniel Nkunzimana I am a python programmer and a student in BYU-Idaho. 
+    I will be graduated within 2026 next year. I  am studying software development online degree program. 
+    my goal is to become full stack software developer. 
+    """
+    st.info(content)
