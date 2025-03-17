0f22a8e720ac91eeff57fd720a99778abf39b021
DanielNkunzimana <mcdanielgolden11@gmail.com>
2025-03-17 20:53:41 +0200

    Day 21: create columns, image, and content

--- main.py
+++ main.py
@@ -15,4 +15,8 @@ with col2:
     my goal is to become full stack software developer. 
     """
     st.info(content)
-st.info("Below you can find some of the apps I have built. feel free to contact me!")
+
+content2 = """
+Below you can find some of the apps I have built. feel free to contact me!
+"""
+st.write(content2)
